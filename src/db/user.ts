bfe5ec1111ee96920926edb90d9c33428e9eb707
Abhishek Dallas <abhishekdallasalpy@gmail.com>
2024-04-14 23:51:10 +0530

    Add user database logic

---
+++ src/db/user.ts
@@ -0,0 +1,37 @@
+import { Context } from "hono";
+import { createPrismaClienWithContext } from ".";
+
+export async function createUser(data: User, ctx: Context) {
+  const prisma = createPrismaClienWithContext(ctx);
+
+  const user = await prisma.user.create({
+    select: {
+      id: true,
+      username: true,
+      password: false,
+    },
+    data: {
+      username: data.username,
+      password: data.password,
+    },
+  });
+
+  return user;
+}
+
+export async function userDoesExists(username: string, ctx: Context) {
+  const prisma = createPrismaClienWithContext(ctx);
+
+  const user = await prisma.user.findFirst({
+    select: {
+      username: true,
+      password: false,
+      id: false,
+    },
+    where: {
+      username,
+    },
+  });
+
+  return user;
+}
