6ed5cb2d20cd73f9fe9b744b3a6cc4c26a60f942
Abhishek Dallas <abhishekdallasalpy@gmail.com>
2024-04-14 23:50:39 +0530

    Add types

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,17 @@ interface Device {
   key: string;
   ip: string;
 }
+
+interface User {
+  username: string;
+  password: string;
+}
+
+enum StatusCode {
+  Ok = 200,
+  BadRequest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
+  ResourceConflict = 409,
+  InternalServerError = 500,
+}
